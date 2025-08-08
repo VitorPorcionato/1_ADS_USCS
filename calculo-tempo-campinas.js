6e71ec14f504a57cbe7940be55e32d3f94df55dd
Vitor Porcionato <129104637+VitorPorcionato@users.noreply.github.com>
2025-08-08 12:27:11 -0300

    Create calculo-tempo-campinas.js

---
+++ calculo-tempo-campinas.js
@@ -0,0 +1,10 @@
+var distanciaKM = 107
+var velocidadeKM = 80
+var tempo = 60
+
+var total = (distanciaKM / velocidadeKM) * 60
+var dist = total / 60
+
+console.log("Exercício 4: ");
+console.log(`O tempo percorrido de São Caetado do Sul á Campinas foi de ${dist.toFixed(2)} horas.`);
+console.log(" ");
