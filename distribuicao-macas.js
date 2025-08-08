8c1a4988aeb2d61d5c66b986b58a303b11abb909
Vitor Porcionato <129104637+VitorPorcionato@users.noreply.github.com>
2025-08-08 12:24:45 -0300

    Create distribuicao-macas.js

---
+++ distribuicao-macas.js
@@ -0,0 +1,22 @@
+var joao = 5 
+var maria = 0 
+var bruxa = 0 
+
+joao +=3;
+
+joao -=2;
+
+joao -=2;
+maria +=2;
+
+maria -=1;
+bruxa +=1;
+
+joao -=1;
+bruxa +=1;
+
+console.log("Exercício 3: ");
+console.log(`João tem ${joao} maças!`);
+console.log(`Maria tem ${maria} maças!`);
+console.log(`Bruxa tem ${bruxa} maças!`);
+console.log(" ");
