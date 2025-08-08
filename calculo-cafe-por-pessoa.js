256214a22399291a8a449932d614ee37482fd503
Vitor Porcionato <129104637+VitorPorcionato@users.noreply.github.com>
2025-08-08 12:22:01 -0300

    Create calculo-cafe-por-pessoa.js

---
+++ calculo-cafe-por-pessoa.js
@@ -0,0 +1,13 @@
+var pessoas = 9
+var cafeML = 200
+
+var mlTotal = pessoas * cafeML
+var litrosTotal = mlTotal / 1000
+
+var colher = 3 
+var colher_litro = litrosTotal * colher
+
+console.log("Exercicio 1:");
+console.log(litrosTotal + " Litros");
+console.log(colher_litro + " Colheres/L");
+console.log(" ");
