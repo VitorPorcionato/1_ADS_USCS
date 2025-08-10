8a2cb15c012ffd4f36b05498c05e2be1fceab5af
Vitor Porcionato <129104637+VitorPorcionato@users.noreply.github.com>
2025-08-10 18:19:52 -0300

    Create calculo-media-customizada.js

---
+++ calculo-media-customizada.js
@@ -0,0 +1,13 @@
+var n1 = parseFloat(prompt("Digite o valor da n1:"));
+var n2 = parseFloat(prompt("Digite o valor da n2:"));
+var n3 = parseFloat(prompt("Digite o valor da n3:"));
+var n4 = parseFloat(prompt("Digite o valor da n4:"));
+var n5 = parseFloat(prompt("Digite o valor da n5:"));
+
+var media = parseFloat(prompt("Digite o valor de deseja ser divisivel:"));
+
+var conta = (n1+n2+n3+n4+n5)/media
+
+console.log("Exercicio 3:");
+console.log(`A média é igual á ${conta}`);
+console.log("");
