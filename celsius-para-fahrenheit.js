acffeb13f44e17c69806157a44d60c903c27d1b4
Vitor Porcionato <129104637+VitorPorcionato@users.noreply.github.com>
2025-08-10 18:16:37 -0300

    Create celsius-para-fahrenheit.js

---
+++ celsius-para-fahrenheit.js
@@ -0,0 +1,6 @@
+console.log("Exercicío 4 pt2:");
+var celsius = prompt("Digite a temperatura em Celsius: ");
+celsius = parseFloat(celsius);
+var F = (9/5) * celsius + 32
+console.log(`Valor em Fahrenheit é: ${F.toFixed(2)}F`);
+console.log("");
