8d8b34d2330bd8ee3ba11c04d26f28ce36c40039
Vitor Porcionato <129104637+VitorPorcionato@users.noreply.github.com>
2025-08-10 18:18:04 -0300

    Create troca-de-valores.js

---
+++ troca-de-valores.js
@@ -0,0 +1,24 @@
+var x = 42
+var y = 7
+
+var temp = x
+x = 7
+y = temp
+
+console.log("Exercício 1pt1:");
+console.log(`Valor de X é igual á ${x} e o valor de Y é igual á ${y}`);
+console.log("");
+
+var x = 42
+var y = 7
+var z = 3 
+
+var temp = x 
+var temp2 = y
+x = temp2
+y = z
+z = temp
+
+console.log("Exercício 1pt2:");
+console.log(`Valor X: ${x}; Valor Y: ${y}; Valor Z: ${z}.`)
+console.log("");
