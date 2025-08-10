e06ff5c606f2fe0fc5abdfe6c3d0b7fe75a882e9
Vitor Porcionato <129104637+VitorPorcionato@users.noreply.github.com>
2025-08-10 18:17:26 -0300

    Create cofre-botao.js

---
+++ cofre-botao.js
@@ -0,0 +1,11 @@
+var botao = 10
+
+botao -=10;
+botao +=5;
+botao -=20;
+botao +=3;
+botao -=10;
+
+console.log("Exercicío 5: ");
+console.log(`O cofre estará em: ${botao}!`);
+vvvvvvvvvvvvvvvvvvvvvvvvv
